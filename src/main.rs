bfb4734569498a758f40ce406589320b1a9f1400
Boris Faure <boris@fau.re>
2023-02-19 19:12:15 +0100

    make it compile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+// Some panic handler needs to be included. This one halts the processor on panic.
+extern crate panic_halt;
+
+use cortex_m_rt::entry;
+
+use stm32_usbd::UsbBus;
+use stm32f0xx_hal::{prelude::*, stm32};
+use usb_device::prelude::*;
+
+use usbd_human_interface_device::device::keyboard::NKROBootKeyboardInterface;
+use usbd_human_interface_device::page::Keyboard;
+use usbd_human_interface_device::prelude::UsbHidClassBuilder;
+
+#[entry]
+fn main() -> ! {
+    let mut dp = stm32::Peripherals::take().unwrap();
+
+    // Reset and clock control
+    let mut rcc = dp
+        .RCC
+        .configure()
+        .hsi48()
+        .enable_crs(dp.CRS) // Clock Recovery System
+        .sysclk(48.mhz())
+        .pclk(24.mhz())
+        .freeze(&mut dp.FLASH);
+
+    let gpioa = dp.GPIOA.split(&mut rcc);
+    let usb_dm = gpioa.pa11;
+    let usb_dp = gpioa.pa12;
+
+    let usb_bus = UsbBus::new(dp.USB, (usb_dm, usb_dp));
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0x0001))
+        .manufacturer("usbd-human-interface-device")
+        .product("NKRO Keyboard")
+        .serial_number("TEST")
+        .build();
+
+    //let mut keyboard = UsbHidClassBuilder::new()
+    //    .add_interface(NKROBootKeyboardInterface::default_config())
+    //    .build(&usb_dev);
+
+    //let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
+    //let mut input_count_down = timer.count_down();
+    //input_count_down.start(10.millis());
+
+    //let mut tick_count_down = timer.count_down();
+    //tick_count_down.start(1.millis());
+
+    loop {
+        //let keys = &[Keyboard::A];
+
+        //keyboard.interface().write_report(keys).ok();
+
+        //tick once per ms
+        //if tick_count_down.wait().is_ok() {
+        //    keyboard.interface().tick().unwrap();
+        //}
+
+        //if usb_dev.poll(&mut [&mut keyboard]) {
+        //    match keyboard.interface().read_report() {
+        //        Ok(l) => {}
+        //        _ => {}
+        //    }
+        //}
+    }
 }
